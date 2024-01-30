0713415f873ebf2d1606c5b8dc1c9ac289593eed
Qu4k3 <paul.guillamon@gmail.com>
2024-01-30 23:02:50 +0100

    ADD Limit dice click every 2 seconds + animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,21 @@ import Dice from './assets/icon-dice.svg?react';
 function App() {
 
   const [advice, setAdvice] = useState({})
-
+  const [isDiceDisabled, setDiceDisabled] = useState(false);
   const windowWidth = useRef(window.innerWidth);
 
   const getAdvice = useCallback(async () => {
     try {
+      setDiceDisabled(true);
       const response = await fetch('https://api.adviceslip.com/advice');
       const data = await response.json();
       setAdvice(data.slip);
     } catch (error) {
       console.error('Error fetching advice:', error);
+    } finally {
+      setTimeout(() => {
+        setDiceDisabled(false);        
+      }, 2000);
     }
   }, []);
 
@@ -29,7 +34,7 @@ function App() {
       <small>ADVICE #{advice.id}</small>
       <p>{advice.advice}</p>
       {windowWidth?.current <= 576 ? <DividerPatternMobile /> : <DividerPatternDesktop />}      
-      <button className="dice" onClick={() => getAdvice()}>
+      <button className="dice" onClick={() => getAdvice()} disabled={isDiceDisabled}>
         <Dice />
       </button>
     </section>
